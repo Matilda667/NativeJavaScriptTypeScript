b12f24419b87f6da6caac32fa277d26c7ee84bfb
Maria <165724856+Matilda667@users.noreply.github.com>
2025-04-16 23:27:16 +0200

    functions

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {splitIntoWords} from "./01-hello-tests/01";
+import {sum} from "./03/03";
 
-
+var a=sum
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
